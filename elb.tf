e08b0c1d6f3aa6fcf40b7721da920ab7078908fa
siva6309 <123646803+siva6309@users.noreply.github.com>
2023-03-19 10:09:33 +0530

    sivashankar

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "siva-terraform-elb"
+  name               = "reddy-terraform-elb"
   availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
