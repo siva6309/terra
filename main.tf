20e40c45953a72cb544975aff6d89890e87fcb0b
siva6309 <123646803+siva6309@users.noreply.github.com>
2023-03-19 11:23:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,42 +74,26 @@ yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
 echo "<!DOCTYPE html>
-<html lang="en-US">
+<html>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+<style>
+body {
+    font-family: 'Sofia';font-size: 22px;
+}
+</style>
+</head>
 <body>
 
-<h1>surya Web Page</h1>
-
-<div id="piechart"></div>
-
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
-<script type="text/javascript">
-// Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-// Draw the chart and set the chart values
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-  ['Task', 'Hours per Day'],
-  ['Work', 8],
-  ['Eat', 2],
-  ['TV', 4],
-  ['Gym', 2],
-  ['Sleep', 8]
-]);
-
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'My Average Day', 'width':550, 'height':400};
-
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}
-</script>
+<h1>Sofia</h1>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+<p>123456790</p>
+<p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+<p>abcdefghijklmnopqrstuvwxyz</p>
 
 </body>
 </html>
+
 "
 EOF
   tags = {
