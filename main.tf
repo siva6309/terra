d8e31e2d3b68c289308e55e714b85be2aefce0ee
siva6309 <123646803+siva6309@users.noreply.github.com>
2023-03-19 11:03:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-0d81306eddc614a45"
   instance_type   = "t2.micro"
-  key_name        = "eks"
+  key_name        = "mumbai"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1b"
   user_data       = <<EOF
@@ -36,7 +36,6 @@ sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my website created by terraform infrastructurte by siva sir server-2" > /var/www/html/index.html
 EOF
   tags = {
     Name = "server-2"
